c6bd2184f1233f9bc0d2cfbc41b98e47b4eeac3b
Adarsh Singh Kushwah <adarshsinghrajawat22@gmail.com>
2024-05-22 00:37:44 +0530

    nsr

--- routes/employee.js
+++ routes/employee.js
@@ -172,9 +172,47 @@ router.post('/submit_employee', function(req, res, next) {
               
                 }
                 });
+
+                //  router.post('/edit_book_data', function(req, res, next) {
+                // try{ 
+                // pool.query('update book set sno=?, d=?, acno=?, rb=?, author=?,title1=?, title2=?, title3=?, subject1=?, subject2=?,edition=?, place=?, publisher=?, year=?, pages=?,volume=?, source1=?, source2=?, billno=?, billdt=?,cost=?, forncost=?, bokkno=? where bookid=?',[req.body.sno, req.body.d, req.body.acno, req.body.rb, req.body.author,req.body.title1, req.body.title2, req.body.title3, req.body.subject1, req.body.subject2,req.body.edition, req.body.place, req.body.publisher, req.body.year, req.body.pages,req.body.volume, req.body.source1, req.body.source2, req.body.billno, req.body.billdt,req.body.cost, req.body.forncost, req.body.bokkno, req.body.bookid],function(error,result){
+                 
+                //   if(error)
+                //   {console.log(error)
+                //        res.status(200).json({status:false,message:'Database Error, Please Contact database admin'})
+                //   }
+                //   else
+                //   {
+                //     res.status(200).json({status:true,message:'Book Updated Successfully'})
+                //   }
+                //  })
+                // }
+                // catch(e)
+                // {
+                //   res.status(200).json({status:false,message:'Server Error....'})
+              
+                // }
+                // });
+
+                router.post('/submit_bookdetail', function(req, res, next) {
+                  try{ 
+                  pool.query('insert into bookdetail (lno, author, title1, title2, title3, edition, place, publisher, volume, year, cost, forncost, qty, noremd, rem, crvno, status, itemcode) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[req.body.lno, req.body.author, req.body.title1, req.body.title2, req.body.title3,req.body.edition, req.body.place, req.body.publisher, req.body.volume, req.body.year,req.body.cost, req.body.forncost, req.body.qty, req.body.noremd, req.body.rem,req.body.crvno, req.body.status, req.body.itemcode],function(error,result){
+                   
+                    if(error)
+                    {console.log(error)
+                         res.status(200).json({status:false,message:'Database Error, Please Contact database admin'})
+                    }
+                    else
+                    {
+                      res.status(200).json({status:true,message:'Book Added Successfully'})
+                    }
+                   })
+                  }
+                  catch(e)
+                  {
+                    res.status(200).json({status:false,message:'Server Error....'})
+                
+                  }
+                  });
   
   module.exports = router;
-
-
-
-
